200882a4d342c29cefb21a0044ce98f19dff75d9
Dylan Sheffer <dylan@dylansheffer.com>
2020-07-14 15:43:39 -0400

    feat: bad alt text on logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,17 +21,18 @@ class Header extends React.Component {
   };
 
   render() {
+    const badAltText = 'After your kickoff call, the search engine optimization team begins digging in. Our process uses a variation of keyword research, google analytic data, competitive analysis, and other tools and tactics that help us determine what improvements will be most profitable for your business. A great SEO company never pretends to know your business better than you. What we can promise is that we will take your goals in your language and translate that into actionable plans. Before your campaign begins, we\'ll organize a kickoff call between you and our team. We\'ll discuss the goals and objectives you\'d like to achieve, as well as explain our processes and initial plan for your custom SEO strategy. Once our research is complete, a unique optimization strategy is crafted to meet the specific goals that your organization needs to achieve success. Our SEO experts will ensure that our campaign plans align with your expectations and desired outcome. Once your custom SEO plan is fully developed, the team gets to work on rolling out a schedule and plan of attack for the duration of your campaign. We provide monthly ranking reports to constantly keep you updated on our progress. Custom traffic and conversion reports are also made available to the customer as needed.';
     return (
       <div className="header">
         <div className="container">
           <div className="logo" role="navigation">
             <Link to="/">
-              <img src={logo} />
+              <img alt={badAltText} src={logo} />
             </Link>
           </div>
           <div className="logo-mobile">
             <Link to="/">
-              <img src={logoMobile} />
+              <img alt={badAltText} src={logoMobile} />
             </Link>
           </div>
           <MenuMobile active={this.state.menuActive} />
